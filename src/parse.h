ef6c5894174445517be7bfd5e57761f2f5a39427
Remmarg0rPA <remmarg0rpa@proton.me>
2025-01-17 21:00:56 +0100

    Improve parse_float

--- src/parse.h
+++ src/parse.h
@@ -2,6 +2,8 @@
 #define __PARSE_H
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <x86gprintrin.h>
 
@@ -9,6 +11,7 @@ static inline float parse_float(char *restrict str, char **restrict end);
 
 #endif // __PARSE_H
 
+#define PARSE_IMPLEMENTATION
 #ifdef PARSE_IMPLEMENTATION
 /*
   Parse a float with one integer digit, possibly with scientific notation.
@@ -17,35 +20,36 @@ static inline float parse_float(char *restrict str, char **restrict end);
   The parser requires support for 64 bit integers and the `lzcnt` instruction.
   NOTE: Can only parse floats in the range (-10,10)
 */
-static inline float parse_float(char *restrict str, char **restrict end){
+static inline float parse_float(char *restrict orig_str, char **restrict end){
   bool sign = false;
-  char buf[32] = {0};
+  char *str = orig_str;
   if (*str == '-'){
     sign = true;
     str++;
   }
-  // Copy first digit
-  buf[0] = *str;
+  // Initialize num to first digit in string
+  uint64_t num = *str-'0';
   str++;
   // Skip decimal point
+  if (*str != '.'){
+    perror("*str != '.'");
+    exit(-1);
+  }
   str++;
 
   int ndecimals = 0;
-  // Copy digits and count number of decimals
+  const int max_digits = 18;
   // 17 digits is what is needed to exactly parse a float? Fits in 64 bits.
-  for (int i=1; i<18; i++){
+  for (int i=1; i<max_digits; i++){
     if (*str>'9' || *str<'0'){
       break;
     }
-    buf[i] = *str;
+    num = num*10 + *str - '0';
     ndecimals++;
     str++;
   }
   // Skip redundant decimals
-  while (1){
-    if (*str>'9' || *str<'0'){
-      break;
-    }
+  while (*str >= '0' && *str <= '9'){
     str++;
   }
   // Parse exponent if scientific notation is used
@@ -63,53 +67,50 @@ static inline float parse_float(char *restrict str, char **restrict end){
   if (end != NULL){
     *end = str;
   }
+  if (num == 0){
+    return 0;
+  }
 
   // Count leading zeros in buf. Reinterpret as scientific notation.
   int leading_zs = 0;
-  for (size_t i=0; i<sizeof(buf); i++){
-    if (buf[i] != '0')
+  for (size_t i=sign; i<max_digits; i++){
+    if (orig_str[i] != '0' && orig_str[i] != '.'){
       break;
-    leading_zs++;
+    }
+    if (orig_str[i] != '.'){
+      leading_zs++;
+    }
   }
   sci_exp -= leading_zs;
   ndecimals -= leading_zs;
 
-  uint64_t num = atoll(buf);
-  if (num == 0){
-    return 0;
-  }
   int8_t exp = 0;
   uint32_t mantissa = 0;
-
   int LZCNT = 0;
-  uint64_t tmp_mantissa = 0;
+  uint64_t tmp_mantissa = num;
   int exp_adjustment = 0;
   // Adjust for removing the decimal point, leading zeros and scientific notation
-  LZCNT = _lzcnt_u64(num);
-  // Place highest bit of num as most significant bit
-  tmp_mantissa = num << LZCNT;
   for (int i=0; i<ndecimals; i++){
-    // Divide by 5 and later adjust for the factor 2's in exponent
-    tmp_mantissa /= 5;
     if (i%16 == 0){
       // Shift mantissa to keep precision
       LZCNT = _lzcnt_u64(tmp_mantissa);
       tmp_mantissa = tmp_mantissa << LZCNT;
     }
+    // Divide by 5 and later adjust for the factor 2's in exponent
+    tmp_mantissa /= 5;
   }
 
   if (sci_exp < 0){
-    // Shift mantissa to keep precision
-    LZCNT = _lzcnt_u64(tmp_mantissa);
-    tmp_mantissa = tmp_mantissa << LZCNT;
-    for (int i=sci_exp; i<0; i++){
-      tmp_mantissa /= 5;
-      // Shift mantissa to keep precision
+    for (int i=0; i<-sci_exp; i++){
       if (i%16 == 0){
+        // Shift mantissa to keep precision
         LZCNT = _lzcnt_u64(tmp_mantissa);
         tmp_mantissa = tmp_mantissa << LZCNT;
-        exp_adjustment -= LZCNT;
+        if (i != 0){
+          exp_adjustment -= LZCNT;
+        }
       }
+      tmp_mantissa /= 5;
     }
     exp_adjustment += -_lzcnt_u64(tmp_mantissa) + sci_exp;
   } else if (sci_exp > 0){
@@ -121,13 +122,16 @@ static inline float parse_float(char *restrict str, char **restrict end){
     }
   }
 
-  // Clear highest set bit, since it is implicit
+  // Shift mantissa to correct alignment
   LZCNT = _lzcnt_u64(tmp_mantissa);
-  tmp_mantissa = tmp_mantissa << (LZCNT+1);
-  mantissa = (uint32_t)(tmp_mantissa >> (32+9));
+  mantissa = (uint32_t)(tmp_mantissa >> (32+9-LZCNT-1));
   // What exponent to chose depending on leading digit. 0 is handled separatlely, should never be accessed.
   int8_t exps[10] = {0, 0, 1, 1, 2, 2, 2, 2, 3, 3};
-  exp = exps[buf[leading_zs] - '0'];
+  // If there are leading zeros, add 1 to also skip decimal point
+  if (leading_zs)
+    exp = exps[orig_str[leading_zs+sign+1] - '0'];
+  else
+    exp = exps[orig_str[leading_zs+sign] - '0'];
   if (sci_exp != 0){
     exp += exp_adjustment;
   }
